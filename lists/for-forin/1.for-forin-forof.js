373af977cb6a0f7c47436986e957c3b6177880f5
rinvel7 <rinavelasquez03@gmail.com>
2022-09-04 21:07:48 -0400

    for-forin-forof-SWAPI

---
+++ lists/for-forin/1.for-forin-forof.js
@@ -0,0 +1,47 @@
+//importamos o arquivo service.js
+const service = require('./service')
+
+async function main() {
+    try{
+        //3.Objetivo tentar iterar os items de objeto retornado de SWAPI 
+        //retornar os so nome que estan dentro de results.
+        const result = await service.obterPessoas('a')
+        const names =[] //variavel onde sera adicionado a lista de SWAPI
+        
+//-------------------------FOR--------------------------------------------       
+        console.time('for')//so para verificar tempo de ejecucao
+        //ENTAO:
+        //iniciamos un bucle,
+        //result = const result = await...
+        //results = objeto retornado de SWAPI en test
+        //.length = verifica as cantidades de esse arreglo        
+        for (let i = 0; i <= result.results.length -1; i++) {
+            const pessoa = result.results[i] //pegar pessoas em a posicao i, oseja desde 0, e que seja adicionada essa lista en uma vaiavel chamada: name
+            names.push(pessoa.name)
+        }
+        
+        console.timeEnd('for') 
+//------------------------------FORIN------------------------------------
+    console.time('forin') //so para verificar tempo de ejecucao
+    for (let i in result.results) {
+        const pessoa = result.results[i]
+        names.push(pessoa.name)
+    }
+        console.timeEnd('forin')
+    //--------------------------FOROF-----------------------
+    console.time('forof')
+    for (pessoa of result.results) {
+        names.push(pessoa.name)
+    }
+    console.timeEnd('forof')
+
+        console.log(`name`, names)
+
+    }catch (error){
+        console.error(`error interno`, error)
+
+    }
+}
+   main()
+
+   //FORIN ES MAIS RAPIDO...
---
+++ lists/for-forin/service.js
@@ -0,0 +1,30 @@
+const axios = require('axios')//importamos axios a nosso projeto
+const URL =  `https://swapi.dev/api/people`
+
+async function obterPessoas (nome) { 
+    //concatenamos a variavel (url) com o que estamos solicitando (nome people) ao api e que passe a informacao em formato json
+    const url = `${URL}/?search=${nome}&format=json` 
+    //axios e uma promise que retorna informacao, e para que passe a informacao na misma linea colocamos await
+    const response = await axios.get(url)
+    // y por utimo, retorna um objeto 
+    return response.data
+}
+//1.Objetivo = testar que funcoe correctamente -> Ele retorno un objeto, con todas as caracteristicas 
+/* obterPessoas('r2')
+    .then(function (resultado) {
+        console.log('resultado', resultado)
+    })
+    .catch(function (error) {
+        console.error('DEU RUIM', error)
+    })
+ */
+
+    //2.Objetivo = convertir arquivo que retorno o teste en modulo
+    //e que os outros arquivos consegan visualizar esse modulo.
+
+    //1. exportar o modulo node.js
+    module.exports = {
+        obterPessoas //2. Vamos reutilizar ese modulo en outro aquivo chamado:1.for-forin-forof.js (copiar codigo)
+    }
+    
+
